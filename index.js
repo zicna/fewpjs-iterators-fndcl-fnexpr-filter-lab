7d349e2a900bcdefa82c3e125af77edf4d6647f8
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-08 09:38:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+  return drivers.filter((driver) => new RegExp(name, "i").test(driver));
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter((driver) => {
+    return driver.startsWith(name);
+  });
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => {
+        return driver["name"] === name
+    })
+}
