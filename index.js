514d2e9313b98a113dfb2a28440dd27e289ba34c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-09 18:01:50 -0500

    commit tree f00a11e1c10bb950eab03f5caffb116f7481a483

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 // Code your solution here
 
+//* challange one
+//* reqex solution
+// function findMatching(drivers, name) {
+//   return drivers.filter((driver) => new RegExp(name, "i").test(driver));
+// }
+
+//* from lesson
+
 function findMatching(drivers, name) {
-  return drivers.filter((driver) => new RegExp(name, "i").test(driver));
+    const driverList = drivers.filter( driver => {
+        return driver.toLowerCase() === name.toLowerCase()})
+        return driverList
 }
 
-function fuzzyMatch(drivers, name) {
-  return drivers.filter((driver) => {
-    return driver.startsWith(name);
-  });
+//* challange two
+
+// function fuzzyMatch(drivers, name) {
+//   return drivers.filter((driver) => {
+//     return driver.startsWith(name);
+//   });
+// }
+//* ReqEx solution
+// function fuzzyMatch(drivers, name) {
+//     // let regEx = /"^" + name/
+//     return drivers.filter(driver => {
+//         return new RegExp("^"+name).test(driver)
+//     })
+// }
+
+//*from lesson
+function fuzzyMatch(array, query) {
+    return array.filter(driver => {
+       return  driver.toLowerCase().indexOf(query.toLowerCase()) === 0 
+    })
 }
+
+
+
 
 function matchName(drivers, name) {
     return drivers.filter(driver => {
